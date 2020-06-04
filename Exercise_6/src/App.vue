030dfd6cb7435e9fae395d3e769fba53a4b7807c
Belyavtsev Artem <Arsainos@gmail.com>
2020-06-04 21:08:15 +0300

    exercise 6 and solution

---
+++ Exercise_6/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <headerComponent />
+        <hr>
+        <div class="row">
+            <serversComponent />
+            <serverInfoComponent />
+        </div>
+        <hr>
+        <footerComponent />
+    </div>
+</template>
+
+<script>
+    import Head from './header.vue';
+    import Footer from './footer.vue';
+    import ServerInfo from './serverInfo.vue';
+    import Servers from './servers.vue';
+
+    export default {
+        components: {
+            'headerComponent': Head,
+            'footerComponent': Footer,
+            'serverInfoComponent': ServerInfo,
+            'serversComponent': Servers
+        }
+    }
+</script>
+
+<style>
+
+</style>
